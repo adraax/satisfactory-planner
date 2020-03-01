c2c466406a51a88be9b7b73a3a64dfdf9971f4f4
adraax <merlin.olivier71@gmail.com>
2020-03-02 00:21:53 +0100

    add selection for resource and quantity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { MatSelect } from "@angular/material/select";
+import { MatInput } from "@angular/material/input";
+import { ReplaySubject, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 import { ResourceService } from "./resource.service";
 
 @Component({
@@ -6,12 +11,74 @@ import { ResourceService } from "./resource.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit {
   title = "satisfactory-planner";
+  resources = [];
+  resourcesName = [];
+  output = {};
+
+  /** control for the selected resource */
+  public resourceCtrl: FormControl = new FormControl();
+  public resourceNumberCtrl: FormControl = new FormControl();
+
+  /** control for the MatSelect filter keyword */
+  public resourceFilterCtrl: FormControl = new FormControl();
+
+  /** list of resources filtered by search keyword */
+  public filteredresources: ReplaySubject<any> = new ReplaySubject<any>(1);
+
+  @ViewChild("singleSelect", { static: true }) singleSelect: MatSelect;
+  @ViewChild("singleSelect", { static: true }) numberInput: MatInput;
+
+  /** Subject that emits when the component has been destroyed. */
+  protected _onDestroy = new Subject<void>();
 
   constructor(private resourceService: ResourceService) {}
 
-  ngAfterViewInit(): void {
-    this.resourceService.getResources().subscribe(data => console.log(data));
+  ngOnInit(): void {
+    this.resourceService.getResources().subscribe((data: any) => {
+      if ("resources" in data) {
+        this.resources = data.resources;
+        this.resourcesName = Object.keys(this.resources);
+        this.initSelect();
+      }
+    });
+  }
+
+  initSelect() {
+    this.resourceCtrl.setValue(this.resourcesName.sort());
+    this.resourceNumberCtrl.setValue(1);
+    this.filteredresources.next(this.resourcesName.slice());
+
+    // listen for search field value changes
+    this.resourceFilterCtrl.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filterResources();
+      });
+  }
+
+  protected filterResources() {
+    if (!this.resources) {
+      return;
+    }
+    // get the search keyword
+    let search = this.resourceFilterCtrl.value;
+    if (!search) {
+      this.filteredresources.next(this.resourcesName.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredresources.next(
+      this.resourcesName.filter(
+        resource => resource.toLowerCase().indexOf(search) > -1
+      )
+    );
+  }
+
+  calculate() {
+    console.log(this.singleSelect.value);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { LayoutModule } from "@angular/cdk/layout";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { MatSelectModule } from "@angular/material/select";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxMatSelectSearchModule } from "ngx-mat-select-search";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { MatOptionModule } from '@angular/material/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +31,15 @@ import { AppComponent } from "./app.component";
     MatIconModule,
     MatListModule,
     FlexLayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatInputModule,
+    MatButtonModule,
+    NgxMatSelectSearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
